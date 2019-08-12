9089813be49818569fb74fd89d4b1596b88ef8ae
Himesh Siriwardana <himeshsiriwardana@Himeshs-MacBook-Air.local>
2019-08-12 11:22:24 -0400

    Updates

---
+++ precache-manifest.8311007430d32e69b9e8a2d8ff0ead13.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "113438ff0aca209ee6abff8a3bc1c324",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5ac007942509cbed734b",
+    "url": "/monsters-rolodex/static/css/main.5d11b22a.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/monsters-rolodex/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "5ac007942509cbed734b",
+    "url": "/monsters-rolodex/static/js/main.0030d703.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
